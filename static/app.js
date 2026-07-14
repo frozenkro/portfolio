f150d9f2481da449f49a80ea5afac9ff1b851ef1
kaup <kalebroyolson+github@gmail.com>
2026-07-13 22:47:12 -0500

    Filler descriptions in .js

--- static/app.js
+++ static/app.js
@@ -7,6 +7,11 @@ const initStamps = () => {
   const messages = {
     'stamp-1': 'Talk about architecting software into focused, testable units.',
     'stamp-2': 'Talk about repeatable environments.',
+    'stamp-3': 'talk about how micro-inefficiencies scale',
+    'stamp-4': 'talk about declarative code',
+    'stamp-5': 'talk about portable outputs (executables, bundles, etc)',
+    'stamp-6': 'Talk about minimizing unnecessary abstractions',
+    'stamp-7': 'Talk about minimizing dependencies and other bloat',
     // ...
   };
 
